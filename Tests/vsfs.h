654b2c3729eb88818bd0153c9bd0e62053fbf84e
RizBits14 <mohammad.rizwanul.islam12014@gmail.com>
2025-05-11 20:04:41 +0600

    Update vsfs.h

--- Tests/vsfs.h
+++ Tests/vsfs.h
@@ -1,29 +1,47 @@
 #ifndef VSFS_H
 #define VSFS_H
 
-#include <stdint.h>
-
 #define BLOCK_SIZE 4096
 #define TOTAL_BLOCKS 64
-#define INODE_BITMAP_BLOCK 1
-#define DATA_BITMAP_BLOCK 2
-#define INODE_TABLE_START_BLOCK 3
-#define INODE_TABLE_BLOCKS 5
-#define DATA_BLOCK_START 8
+#define BLOCK_COUNT TOTAL_BLOCKS
 
-// Superblock structure (Size: 4096 bytes)
-typedef struct {
-    uint16_t magic;
-    uint32_t block_size;
-    uint32_t fs_size;
-    uint32_t inode_bitmap_block;
-    uint32_t data_bitmap_block;
-    uint32_t inode_table_start;
-    uint32_t data_block_start;
-    uint32_t inode_size;
-    uint32_t inode_count;
-    uint8_t reserved[4058];
+#define INODE_SIZE 256
+#define INODE_TABLE_BLOCKS 5
+#define INODES_PER_BLOCK (BLOCK_SIZE / INODE_SIZE)
+#define INODE_COUNT (INODES_PER_BLOCK * INODE_TABLE_BLOCKS)
+
+#define MAGIC 0xD34D
+
+typedef struct superblock {
+    unsigned short magic;             // 2 bytes
+    unsigned int block_size;          // 4 bytes
+    unsigned int block_count;         // 4 bytes
+    unsigned int inode_bitmap_block;  // 4 bytes
+    unsigned int data_bitmap_block;   // 4 bytes
+    unsigned int inode_table_start;   // 4 bytes
+    unsigned int first_data_block;    // 4 bytes
+    unsigned int inode_size;          // 4 bytes
+    unsigned int inode_count;         // 4 bytes
+    char reserved[4058];              // Padding to make 4096 bytes
 } Superblock;
+
+typedef struct inode {
+    unsigned int mode;
+    unsigned int uid;
+    unsigned int gid;
+    unsigned int file_size;
+    unsigned int access_time;
+    unsigned int creation_time;
+    unsigned int modification_time;
+    unsigned int deletion_time;
+    unsigned int links;              
+    unsigned int blocks;
+    unsigned int direct_block;
+    unsigned int single_indirect;
+    unsigned int double_indirect;
+    unsigned int triple_indirect;
+    char reserved[156];               // Padding to 256 bytes
+} Inode;
 
 #endif
 
